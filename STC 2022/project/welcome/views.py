f4d23d9ef805589348415cb773eb84dc87e4f01b
Nicholas Ong <47775170+OneGiantNick@users.noreply.github.com>
2022-10-08 17:40:37 +0800

    Add files via upload

--- STC 2022/project/welcome/views.py
+++ STC 2022/project/welcome/views.py
@@ -10,7 +10,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
-# RINKEBY ACCOUNT
+# Sepolia account
 public_key = ""
 private_key = ""
 
