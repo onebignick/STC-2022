067972ece8198982527cf1f5e46e655aa0bec9e4
Nicholas Ong <ong.zhengkai.nicholas@dhs.sg>
2022-10-16 18:20:54 +0800

    able to interact now huat

--- project/welcome/deploy.py
+++ project/welcome/deploy.py
@@ -1,10 +1,12 @@
 import json
 from web3 import Web3
-
+from pathlib import Path
 from solcx import compile_standard, install_solc
 from web3.middleware import geth_poa_middleware
 
-with open("./db.sol", "r") as file:
+p = Path(__file__).with_name("db.sol")
+
+with p.open("r") as file:
     db_file = file.read()
 
 print("Installing...")
@@ -30,14 +32,12 @@ with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
 
 # Bytecode
-bytecode = compiled_sol["contracts"]["db.sol"]["passwordStorage"]["evm"]["bytecode"][
-    "object"
-]
+bytecode = compiled_sol["contracts"]["db.sol"]["userData"]["evm"]["bytecode"]["object"]
 
 # Abi
-abi = json.loads(compiled_sol["contracts"]["db.sol"]["passwordStorage"]["metadata"])[
-    "output"
-]["abi"]
+abi = json.loads(compiled_sol["contracts"]["db.sol"]["userData"]["metadata"])["output"][
+    "abi"
+]
 
 # For connecting to Sepolia
 # w3 = Web3(Web3.HTTPProvider("https://rpc.sepolia.dev"))
@@ -78,14 +78,44 @@ print("Waiting for transaction to finish...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
 
-
-def get_contract_address():
-    return tx_receipt.contractAddress
+db = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
 
 
-def get_bytecode():
-    return bytecode
+def createNewUser(username, password):
+    nonce = w3.eth.getTransactionCount
+    transaction = db.functions.create_user(username, password).buildTransaction(
+        {
+            "chainId": chain_id,
+            "gasPrice": w3.eth.gas_price,
+            "from": my_address,
+            "nonce": nonce + 1,
+        }
+    )
+    signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    print("Creating new User...")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print("User Created!")
 
 
-def get_abi():
-    return abi
+def updateUser(username, password):
+    nonce = w3.eth.getTransactionCount
+    transaction = db.functions.update_user_password(
+        username, password
+    ).buildTransaction(
+        {
+            "chainId": chain_id,
+            "gasPrice": w3.eth.gas_price,
+            "from": my_address,
+            "nonce": nonce + 1,
+        }
+    )
+    signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    print("Updating password...")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print("Password updated!")
+
+
+def findPassword(username):
+    return db.functions.get_user_password(username).call
