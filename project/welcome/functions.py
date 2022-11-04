cf8d5f128907622a09b567582baab6016adda68a
JONATHAN ONG <211788R@mymail.nyp.edu.sg>
2022-11-04 15:53:27 +0800

    fixed visual bug and consistency

--- project/welcome/functions.py
+++ project/welcome/functions.py
@@ -64,15 +64,15 @@ if chain_id == 11155111:  # Sepolia chain ID is 11155111
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     print(w3.clientVersion)
 
-my_address = "0x1667a684E0bD33EdeCf74EE86B52835312bd7eEA"
-private_key = "c9544cafe50f0cebcb512535e6a902fcae5ecddf7c5fd832d39ef2e645a6be56"
+# my_address = "0x1667a684E0bD33EdeCf74EE86B52835312bd7eEA"
+# private_key = "c9544cafe50f0cebcb512535e6a902fcae5ecddf7c5fd832d39ef2e645a6be56"
 
 # my_address = "0x545AFcA9a28c42e08D670fe72B69376213fbEDa3"
 # private_key = "8ce2763ff0b066ca62ee7e35f6c1e6db977954f1cdaecacf902dc9c3744bd942"
 
 # jons add and key
-# my_address = "0xE5ce067301e150F27F50Eb58ae078A80ab987183"
-# private_key = "36230e823372730c5225d10470fef124aaa6a1a2f4286f78b5eba097c8af0653"
+my_address = "0xE5ce067301e150F27F50Eb58ae078A80ab987183"
+private_key = "36230e823372730c5225d10470fef124aaa6a1a2f4286f78b5eba097c8af0653"
 
 # Get contract address of deployed contract
 p = Path(__file__).with_name("contractaddress.txt")
